5342392edc540916605dfdd6c6fd6ce849c9fd79
zananskij <zananskij@gmail.com>
2022-12-08 14:38:10 -0500

    frontend updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-// import Post from './components/Post'
-// import Add from './components/Add'
+import Post from './components/Post'
+import Add from './components/Add'
 // import Edit from './components/Edit'
 
 const App = () => {
-  const [posts, setPosts] = useState([])
+  const [post, setPost] = useState([])
 
-  const getPosts = () => {
+  const getPost = () => {
     axios
       .get('http://localhost:3000/twitter')
       .then(
-        (response) => setPosts(response.data),
+        (response) => setPost(response.data),
         (err) => console.log(err)
       )
       .catch((error) => console.log(error))
@@ -20,38 +20,40 @@ const App = () => {
   const handleCreate = (data) => {
     axios.post('http://localhost:3000/twitter/' + data._id, data).then((response) => {
       console.log(response)
-      let newPost = posts.map((post) => {
-        return post._id !== data._id ? posts : data
-      })
-      setPosts(newPost)
-    })
-  }
-
-  const handleEdit = (data) => {
-    axios.put('http://localhost:3003/twitter/' + data._id, data).then((response) => {
-      console.log(response)
       let newPost = post.map((post) => {
-        return posts._id !== data._id ? post : data
+        return post._id !== data._id ? post : data
       })
       setPost(newPost)
     })
   }
 
-  const handleDelete = (deletedPost) => {
-    axios.delete('http://localhost:3000/twitter/' + deletedPost._id).then((response) => {
-      let newPost = posts.filter((posts) => {
-        return posts._id !== deletedPost._id
-      })
-    })
-  }
+  // const handleEdit = (data) => {
+  //   axios.put('http://localhost:3003/twitter/' + data._id, data).then((response) => {
+  //     console.log(response)
+  //     let newPost = post.map((post) => {
+  //       return post._id !== data._id ? post : data
+  //     })
+  //     setPost(newPost)
+  //   })
+  // }
+
+  // const handleDelete = (deletedPost) => {
+  //   axios.delete('http://localhost:3000/twitter/' + deletedPost._id).then((response) => {
+  //     let newPost = post.filter((post) => {
+  //       return post._id !== deletedPost._id
+  //     })
+  //   })
+  // }
 
   useEffect(() => {
-    getPosts()
+    getPost()
   }, [])
 
   return (
     <>
       <h1> Twitter Clone</h1>
+      <Post post={post} />
+      <Add handleCreate={handleCreate} />
     </>
   )
 }
