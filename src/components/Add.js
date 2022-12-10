7bb61e8c4b02463b0606ed10eaf936b62a733c2e
levonal <lwshabee@gmail.com>
2022-12-09 18:09:31 -0600

    day2

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,26 @@
-import { useState } from 'react'
+
+import { useEffect, useState } from 'react';
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { geocodeByPlaceId, getLatLng } from 'react-google-places-autocomplete';
 
 const Add = (props) => {
   const [post, setPost] = useState({ post: '', img: '', location: '', date: '' })
+
+  const [value, setValue] = useState(null);
+
+    useEffect(() => {
+        console.log(value)
+
+        if (value) {
+            geocodeByPlaceId(value.value.place_id)
+            .then(results => getLatLng(results[0]))
+            .then(({ lat, lng }) => {
+                setPost({ ...post, locationDisplayName: value.value.structured_formatting.main_text, address: value.label, latitude: lat, longitude: lng })
+                console.log('Successfully got latitude and longitude', { lat, lng })
+            }
+            );
+        }
+    }, [value])
 
   const handleChange = (event) => {
     setPost({ ...post, [event.target.name]: event.target.value })
@@ -17,8 +36,12 @@ const Add = (props) => {
       <div className="mb-5 bg-light 
       m-auto ">
         <form onSubmit={handleSubmit}>
-          <label className='form-label' htmlFor="location ">location : </label>
-          <input className=' form-control ' type="text" name="location" onChange={handleChange} />
+        <label htmlFor='location'>Location</label><br />
+                            <GooglePlacesAutocomplete
+                                selectProps={{
+                                    value,
+                                    onChange: setValue,
+                                }} />
           <br />
           <br />
           <div className='mb-3'>
